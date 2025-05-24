2eee1c103bb5008935a14d6b6743f424cff63e5a
Your Name <gospelachuenu@gmail.com>
2025-05-24 19:56:29 +0100

    Changed root route to serve index.html instead of opening-soon.html

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ async function checkLiveStreamStatus() {
     }
 }
 
-// Serve the Opening Soon page for all root requests
+// Serve the main index page for all root requests
 app.get('/', async (req, res) => {
     const liveStatus = await checkLiveStreamStatus();
-    res.sendFile(path.join(__dirname, 'opening-soon.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Add a new endpoint for live stream status
